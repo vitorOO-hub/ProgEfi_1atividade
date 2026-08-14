df959ee065b354adb6b4c3072143ab63d1a0a8a1
vitorOO <vitoros@al.insper.edu.br>
2026-08-14 00:28:08 -0300

    Separando responsabilidades feitas ate separar html em templates

--- servidor.py
+++ servidor.py
@@ -1,37 +1,20 @@
-from flask import Flask, url_for, render_template_string
-from utils import load_notes
-
+from flask import Flask, render_template_string
+from utils import load_notes, load_template
 
 app = Flask(__name__)
 
 # Configurando a pasta de arquivos estáticos
 app.static_folder = 'static'
 
-RESPONSE_TEMPLATE = '''<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Get-it</title>
-</head>
-<body>
-
-<img src="{{ url_for('static', filename='img/getit.png') }}">
-<p>Como o Post-it, mas com outro verbo</p>
-
-<ul>
-{{notes | safe}}
-</ul>
-
-</body>
-</html>
-'''
-
 @app.route('/')
 def index():
-    notes_html = load_notes()
-    return render_template_string(
-        RESPONSE_TEMPLATE, notes=notes_html
-    )
+
+    notes = load_notes()
+    imagem_getit =  "/static/img/getit.png"
+    response = load_template('static/templates/index.html').format(notes=notes, imagem=imagem_getit)
+
+    return render_template_string(response)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- utils.py
+++ utils.py
@@ -5,10 +5,15 @@ def load_data(nome_arquivo):
         return json.load(arquivo)
     
 def load_notes():
-    notes_data = load_data('static/data/notes_getit.json')
     notes_li = [
-        f"<li><h3>{dados['titulo']}</h3><p>{dados['detalhes']}</p></li>"
-        for dados in notes_data
-    ]
+            load_template('static/templates/components/note.html').format(title=dados['titulo'], details=dados['detalhes'])
+            for dados in load_data('static/data/notes_getit.json')
+        ]
     
-    return '\n'.join(notes_li)
+    notes = '\n'.join(notes_li)
+
+    return notes
+
+def load_template(nome_template):
+    with open(nome_template, "r", encoding="utf-8") as arquivo:
+        return arquivo.read()
